a268414b3df273eb6c35dbb087c2256e0be4f5e3
Lex <mr.x97@mail.ru>
2019-10-04 09:36:51 +0300

    Updated with SimBackup policy

---
+++ src/pages/simbackup-policy.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default () => (
+  <>
+    <h3>Cookies and Policies</h3>
+    <p>
+      SimBackup does not communicate with web server and solely works on your device. Therefore, it does not need any access to cookie or your browser data. Beware of scammers, which may require this from you. We, at SimBackup does not require this from you
+    </p>
+    <h3>Third-party services</h3>
+    <p>
+      SimBackup uses native Android calls and libraries, which do not invoke communication to external web services
+    </p>
+    <h3>Advertisement</h3>
+    <p>
+      SimBackup uses Google AdMob SDK to display you the advertisements. Any future information you can get from their <a href="https://admob.google.com/">documentation</a>
+    </p>
+    <h3>Terms and Conditions</h3>
+    <p>
+      SimBackup may update this privacy policy and update all the provided links to it, so you can read it again and accept or reject changes
+    </p>
+  </>
+)
