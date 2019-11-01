facbf8ef4216f6046dcd46bad4ec14673e2ff995
F97A <mr.x97@mail.ru>
2019-11-01 16:20:12 +0200

    Adapted for mobile the landing page

--- src/pages/colormixer-landing.js
+++ src/pages/colormixer-landing.js
@@ -7,7 +7,7 @@ export default class ColorMixerLanding extends React.Component {
 
         return (
             <main>
-                <img src={ withPrefix('/color_mixer/logo.png') } />
+                <img id='colormixer-logo' src={ withPrefix('/color_mixer/logo.png') } />
                 <p>Simple, yet addictive game, based on the principle of additive mixing of colors. Mix the colors to get the proper color, shown by the platform, coming from the bottom</p>
                 <a href='https://play.google.com/store/apps/details?id=com.color_mixer&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'><img alt='Get it on Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png'/></a>
                 <img id='screenshot-colormixer' src={ withPrefix("/color_mixer/phone-screenshot.png") } />
