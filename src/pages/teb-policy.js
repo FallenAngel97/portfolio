537aa05b2dae7bacc2472a5ff3a9c2e95dfec5ca
Lex <mr.x97@mail.ru>
2021-04-19 18:20:26 +0300

    Added TEB policies

---
+++ src/pages/teb-policy.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const TebPolicy = () => (
+  <>
+    <h3>Cookies and Policies</h3>
+    <p>TEB communicates with your social networks only on your phone and not transferring any data to other parties for saving</p>
+    <h3>Third-party services</h3>
+    <p>TEB uses external libraries and toolkits provided by corresponding official API of social networks used in application. By using TEB you are agreeing with licenses and terms provided by corresponding networks. You can view policies and terms on respective social networks or by contacting their representatives</p>
+    <h3>Advertisement</h3>
+    <p>
+    TEB uses Google AdMob SDK to display you the advertisements. Any future information you can get from their <a href="https://admob.google.com/">documentation</a>
+    </p>
+    <h3>Terms and Conditions</h3>
+    <p>
+    TEB may update this privacy policy and update all the provided links to it, so you can read it again and accept or reject changes
+    </p>
+  </>
+)
+
+export default TebPolicy;
